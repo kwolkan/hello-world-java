96f042184d1741986472f10696407528d34d89c3
Karol Wolkanin <karol.wolkanin@gmail.com>
2020-10-18 22:20:02 +0200

    Initial version

---
+++ src/main/java/com/kwolk/docker_git_hello_world/HelloWorldPing.java
@@ -0,0 +1,17 @@
+package com.kwolk.docker_git_hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorldPing 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+    	for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+
+    }
+}
