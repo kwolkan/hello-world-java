39b2dd57140100db646a15c6809e552843caaffd
Karol Wolkanin <karol.wolkanin@gmail.com>
2020-10-18 22:53:08 +0200

    Updated with new output

--- src/main/java/com/kwolk/docker_git_hello_world/HelloWorldPing.java
+++ src/main/java/com/kwolk/docker_git_hello_world/HelloWorldPing.java
@@ -9,7 +9,7 @@ public class HelloWorldPing
     public static void main( String[] args ) throws InterruptedException
     {
     	for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+			System.out.println("New version of Hello World Ping " + i );
 			Thread.sleep(1000);
 		}
 
